e7a9303c3490d2f81712cf1d980df8fbaba0e387
janine d <jdesrosiers1416@gmail.com>
2023-06-24 13:16:29 -0400

    pom and settings class updates

--- Archive/maven-install.sh
+++ Archive/maven-install.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
-sudo su
-yum update
-amazon-linux-extras install java-openjdk11
+sudo yum update -y
+sudo amazon-linux-extras install java-openjdk11 -y
 java --version
-wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-yum install -y apache-maven
+sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
 
-## Configure MAVEN_HOME 
-echo "MAVEN_HOME=/usr/share/apache-maven" >> .bash_profile
-echo "PATH=$MAVEN_HOME/bin:$PATH" >> .bash_profile
+## Configure MAVEN_HOME and PATH Environment Variables
+rm .bash_profile
+wget https://raw.githubusercontent.com/awanmbandi/realworld-cicd-pipeline-project/jenkins-master-client-config/.bash_profile
 source .bash_profile
 mvn -v
 
-## Provision Jenkins Master Access
+## Provision Jenkins Master User
 sudo su
 useradd jenkinsmaster 
 echo jenkinsmaster | passwd jenkinsmaster --stdin ## Amazon Linux
 
+## Enable Password Authentication and Authorization
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
-echo "jenkinsmaster ALL=(ALL)" >> /etc/sudoers
+echo "jenkinsmaster   ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 chown -R jenkinsmaster:jenkinsmaster /opt
 
-## Install git
+## Install Git SCM
 yum install git -y
 
-## Download settings.xml into .m2 for Authorization
+## Download the settings.xml file into /home/USER/.m2 to provide Authorization to Nexus
 mkdir /home/jenkinsmaster/.m2
 wget https://raw.githubusercontent.com/jdrose14/jenkins-master-client-project/maven-sonarqube-nexus-jenkins/settings.xml -P /home/jenkinsmaster/.m2/
+chown jenkinsmaster:jenkinsmaster /home/jenkinsmaster/.m2/
+chown jenkinsmaster:jenkinsmaster /home/jenkinsmaster/.m2/settings.xml
--- Archive/random.md
+++ Archive/random.md
@@ -4,6 +4,12 @@ mvn clean sonar:sonar \
   -Dsonar.host.url=http://44.203.4.255:9000 \
   -Dsonar.login=<sonarqube prject token>
 
+## Actual maven + sonar
+mvn sonar:sonar \
+  -Dsonar.projectKey=Maven-JavaWebApp \
+  -Dsonar.host.url=http://172.31.88.13:9000 \
+  -Dsonar.login=
+
 ## Before Running the `deploy` Command make sure to update the Nexus pom.xml and settings.xml with Nexus IP addedd, Username and Password. Make sure the plugings are defined as well
 mvn clean package sonar:sonar \
   -Dsonar.projectKey=JavaWebApp-Project3 \
@@ -15,3 +21,6 @@ mvn clean package sonar:sonar deploy \
   -Dsonar.projectKey=JavaWebApp-Project3 \
   -Dsonar.host.url=http://44.203.4.255:9000 \
   -Dsonar.login=<sonarqube prject token>
+
+## Run in Maven Terminal to change to java 11
+sudo alternatives --config java
